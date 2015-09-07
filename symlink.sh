7b2e50cbd9ef9e68f1f2c11544349fead7aa5f41
Joao Vortmann <jvortmann@gmail.com>
2015-09-07 13:06:56 -0300

    feature: Add symlink for nvim configuration

--- symlink.sh
+++ symlink.sh
@@ -4,3 +4,7 @@ echo "Setting symlinks for default configurations"
 ln -sfhv $(pwd) ~/.vim
 ln -sfhv ~/.vim/vimrc ~/.vimrc
 ln -sfhv ~/.vim/gvimrc ~/.gvimrc
+
+echo "Setting symlinks for NVim"
+ln -sfhv ~/.vim ~/.nvim
+ln -sfhv ~/.nvim/vimrc ~/.nvimrc
