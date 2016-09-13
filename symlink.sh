8e34dbefab935e88314cd3bffcd503ce045402fd
João Vortmann <jvortmann@gmail.com>
2016-09-13 18:29:11 -0300

    fix: Removing old configuration before setup and properly setting symlinks

--- symlink.sh
+++ symlink.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-echo "Symlink default configurations"
+echo; echo ">> Removing existing configs"
+rm -r ~/.vim
+
+echo; echo ">> Symlink default configurations"
 ln -sfhv $(pwd) ~/.vim
 ln -sfhv ~/.vim/vimrc ~/.vimrc
 ln -sfhv ~/.vim/gvimrc ~/.gvimrc
-echo
 
-echo "Symlink NVim configuration"
-ln -sfhv ~/.vim ~/.config/nvim
-ln -sfhv ~/.vim/vimrc ~/.config/nvim/init.vim
+echo; echo ">> Creating ~/.config"
+mkdir -p ~/.config
+
+echo; echo ">> Symlink NVim configuration"
+ln -sfhv $(pwd) ~/.config/nvim
+ln -sfhv ~/.config/nvim/vimrc ~/.config/nvim/init.vim
+
 echo
